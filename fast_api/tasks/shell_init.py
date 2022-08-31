c5f0fc7c7bd428d03b3f1e63cb32135ee50ba9cf
Nikolay <nchernov@gmail.com>
2022-08-31 10:38:35 +0300

    feat: use fastapi template

--- fast_api/tasks/shell_init.py
+++ fast_api/tasks/shell_init.py
@@ -1,5 +1,6 @@
 # flake8: noqa F401
 from app import model as m
 from app.database import SessionLocal
+from app.main import app
 
 db = SessionLocal()
---
+++ fast_api/tests/test_main_page.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+
+def test_main_page(client: TestClient):
+    res = client.get('/')
+    assert res
+    app: FastAPI = client.app
+    assert app.docs_url in res.text
+    assert app.redoc_url in res.text
